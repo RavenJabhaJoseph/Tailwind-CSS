f62d7bbea446417cd9e70832c36a9eeeaf08ce13
Raven Jabha Joseph <ravenjabhajoseph@gmail.com>
2021-12-28 16:08:09 +0530

    Gulp and BrowseSync concept failed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ["./src/**/*.css", "./**/*.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
 }
